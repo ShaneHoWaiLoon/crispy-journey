cbf79f096c0e1ed666f8f00709047826e331b39e
Shane.ho <accen-shaneho@gcc.gov.sg>
2025-01-14 14:34:30 +0800

    Updates

--- assets/index-4gS4HSjs.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-pF29GbjI.js";import{M as e}from"./Meta-4u4yrcpn.js";import{F as t,T as a}from"./App-PaWS1seN.js";import"./index-4H5iOCa0.js";import"./index.esm-KDTjNKzW.js";function p(){return r.jsxs(r.Fragment,{children:[r.jsx(e,{title:"page 3"}),r.jsx(t,{children:r.jsx(a,{variant:"h3",children:"Page 3"})})]})}export{p as default};
