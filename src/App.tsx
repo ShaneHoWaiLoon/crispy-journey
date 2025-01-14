14c5aa629afb69d00d7d842502c02a81ea5cec45
Shane.ho <accen-shaneho@gcc.gov.sg>
2025-01-14 14:42:42 +0800

    Update base route name

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <Notifications />
       <HotKeys />
       <SW />
-      <BrowserRouter>
+      <BrowserRouter basename="/crispy-journey">
         <Header />
         <Sidebar />
         <Pages />
